9fa10f7a2556f7fc5c6adcd907b8d633d16d3956
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-13 13:18:13 +0700

    feat: add destination section (static layout)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero/Hero"
+import DestinationSection from "@/components/destination/DestinationSection"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <DestinationSection />
     </>
   )
 }
